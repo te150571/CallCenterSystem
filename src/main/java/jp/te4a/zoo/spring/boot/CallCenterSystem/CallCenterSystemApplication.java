6f32fd3c100ef3ada01ed1ea9434d9c59b37c6a0
150571 <150571@e0601-09.JC-21.local>
2018-06-18 12:25:25 +0900

    Project starting commit.

---
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/CallCenterSystemApplication.java
@@ -0,0 +1,12 @@
+package jp.te4a.zoo.spring.boot.CallCenterSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CallCenterSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CallCenterSystemApplication.class, args);
+	}
+}
---
+++ src/test/java/jp/te4a/zoo/spring/boot/CallCenterSystem/CallCenterSystemApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.te4a.zoo.spring.boot.CallCenterSystem;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CallCenterSystemApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

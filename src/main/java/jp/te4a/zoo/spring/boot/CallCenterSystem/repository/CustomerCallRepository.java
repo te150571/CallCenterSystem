a89525ec7b6ce2dc89ce1d968a30497bb838a476
150571 <150571@e0601-09.JC-21.local>
2018-07-04 16:33:45 +0900

    Query add.

--- src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/repository/CustomerCallRepository.java
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/repository/CustomerCallRepository.java
@@ -10,11 +10,35 @@ import jp.te4a.zoo.spring.boot.CallCenterSystem.bean.CustomerCallBean;
 
 /*
  * 顧客問い合わせ情報データベースRepository
- * "SearchData" 問い合わせ情報の検索
+ * "SearchDataAll" ユーザIDから問い合わせ情報の検索
+ * "SearchDataAllFromDateAfter" ユーザIDから問い合わせ情報の検索（件名指定なし、指定日付以降）
+ * "SearchDataAllFromDateBefore" ユーザIDから問い合わせ情報の検索（件名指定なし、指定日付以前）
+ * "SearchDataAllFromClass" ユーザIDから問い合わせ情報の検索(件名指定)
+ * "SearchDataAllFromClassAndDateAfter" ユーザIDから問い合わせ情報の検索（件名指定、指定日付以降）
+ * "SearchDataAllFromClassAndDateBefore" ユーザIDから問い合わせ情報の検索（件名指定、指定日付以前）
+ * "SearchDataOne" IDから一件だけ抽出
  */
 
 public interface CustomerCallRepository extends JpaRepository<CustomerCallBean, String> {
 	
-	@Query("")
-	public List<String> SearchData(@Param("uName") String uName, @Param("uTel") String uTel, @Param("uAdd") String uAdd);
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.c_id = :uId order by id asc")
+	public List<String> SearchDataAll(@Param("uId") String uId);
+	
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.c_id = :uId and customer.date >= :date order by id asc")
+	public List<String> SearchDataAllFromDateAfter(@Param("uId") String uId, @Param("date") String date);
+	
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.c_id = :uId and customer.date <= :date order by id asc")
+	public List<String> SearchDataAllFromDateBefore(@Param("uId") String uId, @Param("date") String date);
+	
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.c_id = :uId and customer.class_id <= :classId order by id asc")
+	public List<String> SearchDataAllFromClass(@Param("uId") String uId, @Param("classId") String classId);
+	
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.c_id = :uId and customer.class_id <= :classId and customer.date >= :date order by id asc")
+	public List<String> SearchDataAllFromClassAndDateAfter(@Param("uId") String uId, @Param("classId") String classId, @Param("date") String date);
+	
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.c_id = :uId and customer.class_id <= :classId and customer.date <= :date order by id asc")
+	public List<String> SearchDataAllFromClassAndDateBefore(@Param("uId") String uId, @Param("classId") String classId, @Param("date") String date);
+	
+	@Query("select customer.id, customer.class_id, customer.content, customer.date from CustomerCallBean where customer.id = :cId order by id asc")
+	public List<String> SearchDataOne(@Param("cId") String cId);
 }
--- src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/service/CustomerService.java
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/service/CustomerService.java
@@ -2,6 +2,7 @@ package jp.te4a.zoo.spring.boot.CallCenterSystem.service;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Service;
 
 import jp.te4a.zoo.spring.boot.CallCenterSystem.bean.CustomerBean;
@@ -11,6 +12,7 @@ import jp.te4a.zoo.spring.boot.CallCenterSystem.repository.CustomerRepository;
 /*
  * 顧客情報データベースService
  * "create" 新規顧客登録
+ * "SearchCustomerId" 入力された「苗字」「名前」「電話番号」「住所」をもとに顧客IDを検索
  */
 
 @Service
@@ -25,5 +27,9 @@ public class CustomerService {
 		customerRepository.save(customerBean);
 		customerRepository.flush();
 		return customerForm;
+	}
+	
+	public String SearchCustomerId(String uLastName, String uFirstName, String uTel, String uAdd) {
+		return customerRepository.SearchCustomerId(uLastName, uFirstName, uTel, uAdd);
 	}
 }

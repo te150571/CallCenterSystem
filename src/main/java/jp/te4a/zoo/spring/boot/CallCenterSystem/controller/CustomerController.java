4ff37df3acdbeaa01de263c9ddc844412d1af17e
150571 <150571@e0601-09.JC-21.local>
2018-07-19 09:50:06 +0900

    commit.

--- src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CustomerController.java
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CustomerController.java
@@ -1,7 +1,9 @@
 package jp.te4a.zoo.spring.boot.CallCenterSystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /*
  * 顧客データ操作に関するコントローラ
@@ -12,8 +14,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("customer")
 public class CustomerController {
 
-	@RequestMapping("add")
+	@RequestMapping
 	String customerAdd() {
-		return "";
+		return "operation/client-input";
+	}
+
+	@RequestMapping("check")
+	String customerCheck(
+			@RequestParam("lastName") String lastName,
+			@RequestParam("firstName") String firstName,
+			@RequestParam("lastNameKana") String lastNameKana,
+			@RequestParam("firstNameKana") String firstNameKana,
+			@RequestParam("address") String address,
+			@RequestParam("tel") String tel,
+			@RequestParam("birthday") String birthday,
+			Model model) {
+		
+		
+		
+		return "operation/client-input-check";
 	}
 }

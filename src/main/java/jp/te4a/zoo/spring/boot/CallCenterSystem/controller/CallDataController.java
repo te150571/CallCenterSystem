332753a07e29c0dbf0019a44a57a55e7e0ca9dca
tejc150161 <tejc150161@gmail.com>
2018-08-29 14:49:25 +0900

    問い合わせ内容登録画面遷移＿完了

--- src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CallDataController.java
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CallDataController.java
@@ -4,11 +4,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import jp.te4a.zoo.spring.boot.CallCenterSystem.bean.CustomerCallBean;
+import jp.te4a.zoo.spring.boot.CallCenterSystem.form.CustomerCallForm;
 import jp.te4a.zoo.spring.boot.CallCenterSystem.service.CustomerCallService;
 
 /*
@@ -31,9 +34,19 @@ public class CallDataController {
 	}
 	
 	@RequestMapping("check")
-	String inquiry_create_check(/*@RequestParam("cId") String cId, Model model*/) {
-		
-		
+	String inquiry_create_check() {	/*@RequestParam("cId") String cId, Model model*/
 		return "/operation/query_input_check";
 	}
+	
+	
+	@RequestMapping(value="registration_back", params="back")
+	String inquiry_create_back() {
+		return "/operation/query_input";
+	}
+	
+	@RequestMapping(value="registration_back", params="registration")
+	String inquiry_create_registration() {
+		return "/operation/client-inout-done";
+	}
+	
 }

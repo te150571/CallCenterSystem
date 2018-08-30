bf7c4f4ed4d9b07f0f0dbd4cc5a115532bdc8864
150571 <150571@e0601-09.JC-21.local>
2018-08-30 15:04:53 +0900

    commiiiiiiiit

--- src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CustomerController.java
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CustomerController.java
@@ -36,44 +36,42 @@ public class CustomerController {
 		model.addAttribute("month", month);
 		model.addAttribute("day", day);
 		
-		System.out.println("DEBUG 1 : " + customerForm.getLastname());
-		System.out.println("DEBUG 1 : " + customerForm.getFirstname());
-		System.out.println("DEBUG 1 : " + customerForm.getLastname_kana());
-		System.out.println("DEBUG 1 : " + customerForm.getFirstname_kana());
-		System.out.println("DEBUG 1 : " + customerForm.getBirth());
-		System.out.println("DEBUG 1 : " + customerForm.getTel());
-		System.out.println("DEBUG 1 : " + customerForm.getAddresscode());
-		System.out.println("DEBUG 1 : " + customerForm.getAddress());
-		System.out.println("DEBUG 1 : " + year);
-		System.out.println("DEBUG 1 : " + month);
-		System.out.println("DEBUG 1 : " + day);
-		
 		return "operation/client-input-check";
 	}
 	
 	@RequestMapping("confirm")
-	String customerConfirm(@ModelAttribute("customerForm") CustomerForm customerForm, Model model) {
-		model.addAttribute("customerForm", customerForm);
-		
-		String maxUId = customerService.getMaxId();
-		int maxUIdNum = Integer.parseInt(maxUId);
-		maxUIdNum++;
-		String nextUId = String.format("%08d", maxUIdNum); 
-		customerForm.setId(nextUId);
-		
-		System.out.println("DEBUG 2 : " + customerForm.getId());
-		System.out.println("DEBUG 2 : " + customerForm.getLastname());
-		System.out.println("DEBUG 2 : " + customerForm.getFirstname());
-		System.out.println("DEBUG 2 : " + customerForm.getLastname_kana());
-		System.out.println("DEBUG 2 : " + customerForm.getFirstname_kana());
-		System.out.println("DEBUG 2 : " + customerForm.getBirth());
-		System.out.println("DEBUG 2 : " + customerForm.getTel());
-		System.out.println("DEBUG 2 : " + customerForm.getAddresscode());
-		System.out.println("DEBUG 2 : " + customerForm.getAddress());
-		
-		customerService.create(customerForm);
-		
-		return "operation/client-input-done";
+	String customerConfirm(@ModelAttribute("customerForm") CustomerForm customerForm, @RequestParam(name= "year") int year, @RequestParam(name = "month") int month, @RequestParam(name = "day") int day, @RequestParam(name="switch") String switchFlag, Model model) {
+
+		if(switchFlag.equals("redo")) {
+			model.addAttribute("customerForm", customerForm);
+			model.addAttribute("year", year);
+			model.addAttribute("month", month);
+			model.addAttribute("day", day);
+			
+			return "redirect:redo";
+		}
+		else {
+			String maxUId = customerService.getMaxId();
+			int maxUIdNum = Integer.parseInt(maxUId);
+			maxUIdNum++;
+			String nextUId = String.format("%08d", maxUIdNum); 
+			customerForm.setId(nextUId);
+			
+			System.out.println("DEBUG 2 : " + switchFlag);
+			System.out.println("DEBUG 2 : " + customerForm.getId());
+			System.out.println("DEBUG 2 : " + customerForm.getLastname());
+			System.out.println("DEBUG 2 : " + customerForm.getFirstname());
+			System.out.println("DEBUG 2 : " + customerForm.getLastname_kana());
+			System.out.println("DEBUG 2 : " + customerForm.getFirstname_kana());
+			System.out.println("DEBUG 2 : " + customerForm.getBirth());
+			System.out.println("DEBUG 2 : " + customerForm.getTel());
+			System.out.println("DEBUG 2 : " + customerForm.getAddresscode());
+			System.out.println("DEBUG 2 : " + customerForm.getAddress());
+			
+			customerService.create(customerForm);
+			
+			return "operation/client-input-done";
+		}
 	}
 	
 	@RequestMapping("redo")

0dd355a958f73dc113cfdbea95989f9ecfcf16d9
tejc150161 <tejc150161@gmail.com>
2018-09-04 13:37:57 +0900

    2018/09/04

--- src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CallDataController.java
+++ src/main/java/jp/te4a/zoo/spring/boot/CallCenterSystem/controller/CallDataController.java
@@ -14,6 +14,9 @@ import jp.te4a.zoo.spring.boot.CallCenterSystem.bean.CustomerCallBean;
 import jp.te4a.zoo.spring.boot.CallCenterSystem.form.CustomerCallForm;
 import jp.te4a.zoo.spring.boot.CallCenterSystem.service.CustomerCallService;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 /*
  * 問い合わせ情報に関するコントローラ
  * アドレスは calldata
@@ -34,19 +37,46 @@ public class CallDataController {
 	}
 	
 	@RequestMapping("check")
-	String inquiry_create_check() {	/*@RequestParam("cId") String cId, Model model*/
+	String inquiry_create_check(@RequestParam("cId") String cId, @RequestParam(name = "category") String category, @RequestParam(name = "inquiry_input") String inquiry_input, Model model) {	/*@RequestParam("cId") String cId, Model model*/
+		System.out.println("cId :"+ cId);
+		System.out.println("category :"+ category);
+		System.out.println("check_inquiry_input :" + inquiry_input);
+		
+		model.addAttribute("cId", cId);
+		model.addAttribute("category", category);
+		model.addAttribute("inquiry_input", inquiry_input);
+		
 		return "/operation/query_input_check";
 	}
 	
 	
 	@RequestMapping(value="registration_back", params="back")
-	String inquiry_create_back() {
+	String inquiry_create_back(@RequestParam("cId") String cId, @RequestParam(name = "category_check") String category_check, @RequestParam(name = "inquiry_input") String inquiry_input, Model model) {
+		System.out.println("cId :"+ cId);
+		System.out.println("category_check :"+ category_check);
+		System.out.println("check_inquiry_input :" + inquiry_input);
+		
+		model.addAttribute("cId", cId);
+		model.addAttribute("category_check", category_check);
+		model.addAttribute("inquiry_input", inquiry_input);
 		return "/operation/query_input";
 	}
 	
-	@RequestMapping(value="registration_back", params="registration")
-	String inquiry_create_registration() {
-		return "/operation/client-inout-done";
+	
+	@RequestMapping(value="registration_back", params="confirm")
+	String inquiry_create_registration(@ModelAttribute("customercallForm") CustomerCallForm customercallForm,  Model model) {
+		/*
+		model.addAttribute("uId", uId);*/
+		model.addAttribute("coustomercallForm", customercallForm);
+		
+		// 日付・日時取得
+		Calendar cl = Calendar.getInstance();
+		SimpleDateFormat timedate = new SimpleDateFormat("yyyy/MM/dd k:mm");
+		System.out.println("model : "+model);
+		
+		
+		
+		return "/operation/s-result";
 	}
 	
 }
